46b1d3748c60b89c1385ebd6e61cae1c39009871
Livada86Yelena <livadalenakolya@gmail.com>
2022-01-22 22:12:17 +0200

    Вносит правки

--- js/menu.js
+++ js/menu.js
@@ -3,12 +3,14 @@
      const mobileMenuRef = document.querySelector("[data-menu]");
     
   menuBtnRef.addEventListener("click", () => {
-   // const expanded =
+    const expanded =
       menuBtnRef.getAttribute("aria-expanded") === "true" || false;
     menuBtnRef.classList.toggle("is-open");
        menuBtnRef.setAttribute("aria-expanded", !expanded);
       
      mobileMenuRef.classList.toggle("is-open");
+
+     
      
   });
 })();
